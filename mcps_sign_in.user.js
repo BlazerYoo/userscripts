de6a6b262369bb9c6a99f15cd472f7201c74146b
BlazerYoo <blazeryoo1921@gmail.com>
2021-02-10 02:44:04 -0500

    Create mcps_sign_in.user.js

---
+++ mcps_sign_in.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         MCPS Canvas Sign in Page
+// @namespace    https://github.com/BlazerYoo
+// @version      0.1
+// @description  Auto sign in page for MCPS Canvas
+// @author       BlazerYoo
+// @match        https://classroom.mcpsmd.org/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.location.href = 'https://mcpsmd.instructure.com/login/saml/4';
+})();
