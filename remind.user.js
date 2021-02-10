2e37cd5b067e35997a0df9484d76c32530d4d793
BlazerYoo <blazeryoo1921@gmail.com>
2021-02-10 02:44:54 -0500

    Create remind.user.js

---
+++ remind.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name         Remind Sign in Page
+// @namespace    https://github.com/BlazerYoo
+// @version      0.1
+// @description  Auto sign in page for Remind
+// @author       BlazerYoo
+// @match        https://www.remind.com/
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    window.location.href = 'https://www.remind.com/log_in';
+})();
